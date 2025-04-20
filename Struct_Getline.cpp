b13c6bc09f2493b0e390d0aa247f4217435775cc
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-04-20 16:09:52 +0700

    menambahkan object struct dalam array

--- Struct_Getline.cpp
+++ Struct_Getline.cpp
@@ -25,7 +25,7 @@ int main()
         cin >> mhs[i].nim;
         cin.ignore();
         cout << "Masukkan nama = ";
-        getline(cin, mhs[i].nama);
+        cin >> mhs[i].nama;
         cout << "Masukkan kota = ";
         cin >> mhs[i].alamat.kota;
         cout << "Masukkan provinsi = ";
@@ -36,7 +36,7 @@ int main()
     cout << endl;
     cout << "Menampilkan Data" << endl;
     cout << endl;
-    for (int i = 0; i < 2 i++)
+    for (int i = 0; i < 2; i++)
     {
         cout << "Mahasiswa ke-" << i + 1 << endl;
         cout << "NIM = " << mhs[i].nim << endl;

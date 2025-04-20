7c12d7d6006be17cf39a56d12c75faed0f3acc5b
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-04-20 15:54:16 +0700

    menambahkan object struct dalam array

---
+++ Struct_Array.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+struct Mahasiswa
+{
+    string nama;
+    string nim;
+    string alamat;
+};
+
+int main()
+{
+    // Membuat object struct dalam array
+    Mahasiswa mhs[4];
+    for (int i = 0; i < 4; i++)
+    { 
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "Masukkan nim = ";
+        cin >> mhs[i].nim;
+        cout << "Masukkan nama = ";
+        cin >> mhs[i].nama;
+        cout << "Masukkan alamat = ";
+        cin >> mhs[i].alamat;
+    }
+
+    cout << endl;
+    cout << "Menampilkan Data" << endl;
+    cout << endl;
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "NIM = " <<mhs[i].nim << endl;
+        cout << "Nama = " << mhs[i].nama << endl;
+        cout << "Alamat = " <<mhs[i].alamat << endl;
+    }
+}

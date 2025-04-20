c0e1cfd38caf0a36ecae2aeaaba08d2bc747feb8
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-04-20 15:31:31 +0700

    menambahkan looping for

--- Array.cpp
+++ Array.cpp
@@ -16,4 +16,20 @@ int main()
     cout << "Nilai array ke 3 = " << nilai[2] << endl;
     nilai[2] = 100;
     cout << "Nilai array ke 3 = " << nilai[2] << endl;
+
+    // mengisi array dengan menggunakan looping for
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "Masukkan nama ke " << i + 1 << " = ";
+        cin >> nama[i];
+    }
+
+    cout << endl;
+    cout << "Menampilkan Array Nama" << endl;
+    cout << endl;
+
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "Nama ke " <<i + 1 << " = " << nama[i] << endl;
+    }
 }
